19080876edb1e6796db2ca2afdcef64627b17b98
romax94 <romax1612@gmail.com>
2017-06-03 14:23:12 +0300

    init project and create api

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './containers/App';
+import './index.css';
+
+render(
+    <App />, 
+    document.getElementById('root')
+);
